895986f538177f3f9d6691839934661364eb1f07
Aleksandar Radunovic <aleksandar@radunovic.io>
2018-04-03 15:02:47 +0200

    Pre-publish cleanup & improvements

--- lib/fend/version.rb
+++ lib/fend/version.rb
@@ -1,3 +1,13 @@
 class Fend
-  VERSION = "0.1.0"
+  def self.version
+    VERSION::STRING
+  end
+
+  module VERSION
+    MAJOR = 0
+    MINOR = 1
+    PATCH = 0
+
+    STRING = [MAJOR, MINOR, PATCH].compact.join(".")
+  end
 end
