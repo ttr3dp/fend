14a11ad5539388c792421777b9f2cc9d8be0a020
Aleksandar Radunovic <aleksandar@radunovic.io>
2018-04-06 17:12:19 +0200

    Document full messages plugin

--- lib/fend/plugins/full_messages.rb
+++ lib/fend/plugins/full_messages.rb
@@ -2,6 +2,39 @@
 
 class Fend
   module Plugins
+    # `full_messages` plugin adds `#full_messages` method to `Result` which
+    # returns error messages with prependend param name
+    #
+    #      class UserValidation < Fend
+    #        plugin :full_messages
+    #
+    #        # ...
+    #      end
+    #      result = UserValidation.call(email: "invalid", profile: "invalid", address: { })
+    #
+    #      result.full_messages
+    #      #=> { email: ["email is in invalid format"], profile: ["profile must be hash"], address: { city: ["city must be string"] } }
+    #
+    # ## Array members
+    #
+    # When validating array elements, messages will be returned with prependend
+    # index, since array members don't have a name.
+    #
+    #      { tags: { 0 => ["0 must be string"] } }
+    #
+    # In order to make full messages nicer for array elements,
+    # pass `:array_memeber_names` option when loading the plugin:
+    #
+    #      plugin :full_messages, array_member_names: { tags: :tag }
+    #
+    #      # which will produce
+    #      { tags: { 0 => ["tag must be string"] } }
+    #
+    # `:array_member_names` options is inheritable, so it's possible to define
+    # it globaly by loading the plugin directly through `Fend` class.
+    #
+    #     Fend.plugin :full_messages, array_member_names: { octopi: :octopus }
+    #
     module FullMessages
       def self.configure(validation, opts = {})
         validation.opts[:full_messages_array_member_names] = (validation.opts[:full_messages_array_member_names] || {}).merge(opts[:array_member_names] || {})
