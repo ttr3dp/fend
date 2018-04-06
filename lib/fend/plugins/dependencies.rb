2f254841f812efbcd3759be4a2d738b1c87cf757
Aleksandar Radunovic <aleksandar@radunovic.io>
2018-04-06 16:33:04 +0200

    Document dependencies plugin

--- lib/fend/plugins/dependencies.rb
+++ lib/fend/plugins/dependencies.rb
@@ -2,6 +2,95 @@
 
 class Fend
   module Plugins
+    # `dependencies` plugin enables you to register validation dependencies and
+    # later resolve them so they would be available in validation block
+    #
+    #     plugin :dependencies
+    #
+    # ## Registering dependencies
+    #
+    # There are two types of dependencies:
+    #
+    # 1. **Inheritable dependencies** - available in current validation class
+    #                                   and in subclasses
+    # 2. **Local dependencies** - available only in current validation class
+    #
+    # ### Inheritable dependencies
+    #
+    # Inheritable dependencies can be registered when plugin is loaded:
+    #
+    #     plugin :dependencies, user_class: User
+    #
+    # **Global dependencies** can be registered by loading the plugin directly in
+    # `Fend` class:
+    #
+    #     require "address_checker"
+    #
+    #     Fend.plugin :dependencies, address_checker: AddressChecker.new
+    #
+    # Now, all `Fend` subclasses will be able to resolve `address_checker`
+    #
+    # ### Local dependencies
+    #
+    # Local dependencies can be registered in `deps` registry, on instance level.
+    # Recommended place to do this is the initializer.
+    #
+    #     class UserValidation < Fend
+    #       plugin :dependencies
+    #
+    #       def initialize(model)
+    #         # you can pass a dependency on initialization
+    #         deps[:model] = model
+    #
+    #         # or
+    #
+    #         # hardcode it yourself
+    #         deps[:address_checker] = AddressChecker.new
+    #       end
+    #     end
+    #
+    #     user_validation = UserValidation.new(User)
+    #
+    # ## Resolving dependencies
+    #
+    # To resolve dependencies, `:inject` option needs to be provided to the
+    # `validate` method, with a list of keys representing dependency names:
+    #
+    #     class UserValidation < Fend
+    #       plugin :dependencies, user_model: User
+    #
+    #       validate(inject: [:user_model, :address_checker]) do |i, user_model, address_checker|
+    #         user_model #=> User
+    #         address_checker #=> #<AddressChecker ...>
+    #       end
+    #
+    #       def initialize(address_checker)
+    #         deps[:address_checker] = address_checker
+    #       end
+    #     end
+    #
+    # ## Overriding inheritable dependencies
+    #
+    # To override inheritable dependency, just load the plugin again in a
+    # subclass, or define local dependency with the same name.
+    #
+    #     plugin :dependencies, user_model: SpecialUser
+    #
+    #     # or
+    #
+    #     def initialize
+    #       deps[:user_model] = SpecialUser
+    #     end
+    #
+    # ## Example usage
+    #
+    # Here's an example of email uniqueness validation:
+    #
+    #     validate(inject: [:user_model]) do |i, user_model|
+    #       i.param(:email) do |email|
+    #         email.add_error("must be unique") if user_model.exists?(email: email.value)
+    #       end
+    #     end
     module Dependencies
       def self.configure(validation, opts = {})
         validation.opts[:dependencies] = (validation.opts[:dependencies] || {}).merge(opts)
